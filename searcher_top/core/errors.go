19543da82270cda6e7fc33ba04a75a8b96ba4f76
Varvara Malina <malina.varvara@mail.ru>
2026-05-25 22:21:59 +0300

    chore: add error definitions and model structures for search events and top items

--- searcher_top/core/errors.go
+++ searcher_top/core/errors.go
@@ -1,1 +1,9 @@
 package core
+
+import "errors"
+
+var (
+	ErrEmptyQuery   = errors.New("query must not be empty")
+	ErrEmptyWord    = errors.New("stop word must not be empty")
+	ErrInvalidLimit = errors.New("limit must be between 1 and max_top_limit")
+)
--- searcher_top/core/models.go
+++ searcher_top/core/models.go
@@ -1,1 +1,14 @@
 package core
+
+import "time"
+
+type SearchEvent struct {
+	Query     string
+	UserID    string
+	Timestamp time.Time
+}
+
+type TopItem struct {
+	Query string
+	Count int64
+}
